e2c9bdc933ec8b869d629d2a24b8f1f5970c1022
andyaydna <55512086+andyaydna@users.noreply.github.com>
2021-03-24 21:44:56 -0700

    lab9

---
+++ dashboard-ui/src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.png';
+import './App.css';
+
+import EndpointAudit from './components/EndpointAudit'
+import AppStats from './components/AppStats'
+
+function App() {
+
+    // lab 9
+    const endpoints = ["get_new_cases", "get_new_vaccinated"]
+
+    const rendered_endpoints = endpoints.map((endpoint) => {
+        return <EndpointAudit key={endpoint} endpoint={endpoint}/>
+    })
+
+    return (
+        <div className="App">
+            <img src={logo} className="App-logo" alt="logo" height="150px" width="400px"/>
+            <div>
+                <AppStats/>
+                <h1>Audit Endpoints</h1>
+                {rendered_endpoints}
+            </div>
+        </div>
+    );
+
+}
+
+export default App;
